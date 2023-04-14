1fefbdfd6204f9c1d2a6716974147e078e9c524b
Dmitiy <dimapermyakov55@gmail.com>
2023-04-15 01:46:14 +0300

    Error

--- WoodGrowthCourseWorkSwiftUI/WoodGrowthCourseWorkSwiftUI/Views/MenuButtonsViews/TypeTree/Module/TypeTreesModul.swift
+++ WoodGrowthCourseWorkSwiftUI/WoodGrowthCourseWorkSwiftUI/Views/MenuButtonsViews/TypeTree/Module/TypeTreesModul.swift
@@ -10,20 +10,29 @@ import Foundation
 
 class TypeTreesData: ObservableObject {
     @Published var types: [TypeTreesResult] = []
-    @Published var status                   = true
+    @Published var status                   = false
     
     init() {
+        refresh()
+    }
+    
+    func refresh() {
         APIManager.shared.getTypesTrees {data, error in
             guard let data = data else {
                 print("== ERROR", error!)
                 self.status = false
                 return
             }
+            var tempData: [TypeTreesResult] = []
             for el in data.rows {
                 let info = TypeTreesResult(id: el.type_id, nameType: el.name_type, notes: el.notes, firtilizerName: el.fertilizer_name, plotName: el.plot_name, countTrees: el.count_trees, photo: el.photo)
-                self.types.append(info)
+                tempData.append(info)
             }
-            self.status = true
+            
+            DispatchQueue.main.async {
+                self.types = tempData
+                self.status = true
+            }
         }
     }
 }
